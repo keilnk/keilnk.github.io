afb9f7ecd21b4bbf1d5497e14b14f117496d5b1b
Taoq <106382280+keilnk@users.noreply.github.com>
2025-02-21 23:44:28 +0100

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function searchFunction() {
-    let query = document.getElementById('search-input').value;
-    alert("Searching for: " + query); // You can replace this with real search functionality
-}
