8fe7c3fdb1934564164c3d597fa4e00711e92744
Taoq <106382280+keilnk@users.noreply.github.com>
2025-02-21 23:47:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function searchFunction() {
+    let query = document.getElementById('search-input').value;
+    alert("Searching for: " + query); // You can replace this with real search functionality
+}
